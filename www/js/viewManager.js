36897b0bb3de0047801487fbdb5d582f2ae49b90
RalphSadalski <kontakt@ralphsadalski.de>
2013-04-09 13:58:50 +0300

    Update viewManager.js

--- www/js/viewManager.js
+++ www/js/viewManager.js
@@ -14,33 +14,51 @@ var viewManager = {
 		var URL = "views/" + localURL;
 		var time = 0;
 		
-		$.ajax({
-		    url: URL, // relative path to www folder
-		    type:"get",
-		    dataType:"html",
-		    beforeSend: function ()
-		    {
-		    	time = new Date();
-		    },
-		    success: function(view)
-		    {
-		        $(body).empty();
-		        $(body).append(view);
-		    },
-		    complete: function()
-		    {
-		    	var now = new Date();
-		    	var duration = now - time;
-		    	
-		    	calcTime('loadView: "' + localURL + '", Duration: ' + duration + ' ms');
-		    },
-		    error: function ()
-		    {
-		    	var now = new Date();
-		    	var duration = now - time;
-		    	
-		    	calcTime('loadView: "' + localURL + '" failed, Duration: ' + duration + ' ms');
-		    }
-		});
+		var Storage = localStorage.getItem(localURL);
+		
+		if(Storage == null)
+		{		
+			$.ajax({
+			    url: URL, // relative path to www folder
+			    type:"get",
+			    dataType:"html",
+			    beforeSend: function ()
+			    {
+			    	time = new Date();
+			    },
+			    success: function(view)
+			    {
+			    	localStorage.setItem(localURL, view);
+			    	Storage = localStorage.getItem(localURL);
+			    	
+			        $(body).empty();
+				$(body).append(Storage);
+			    },
+			    complete: function()
+			    {
+			    	var now = new Date();
+			    	var duration = now - time;
+			    	
+			    	calcTime('loadView: "' + localURL + '" from ajax, Duration: ' + duration + ' ms');
+			    },
+			    error: function ()
+			    {
+			    	var now = new Date();
+			    	var duration = now - time;
+			    	
+			    	calcTime('loadView: "' + localURL + '" failed, Duration: ' + duration + ' ms');
+			    }
+			});
+		}
+		else
+		{
+			$(body).empty();
+			$(body).append(Storage);
+			
+			var now = new Date();
+			var duration = now - time;
+			    	
+			calcTime('loadView: "' + localURL + '" from localStorage, Duration: ' + duration + ' ms');
+		}
 	}
 }
